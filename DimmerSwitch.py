7eb31797cece47125b766fe5d8b3c3b3a4f07c51
kresov <kresovivan@mail.ru>
2025-03-13 01:08:47 +0300

    test python1

--- DimmerSwitch.py
+++ DimmerSwitch.py
@@ -1,8 +1,9 @@
 #Класс DimmerSwitch
 class DimmerSwitch():
-    def __init__(self):
-         self.switchIsOn = False
-         self.brightness = 0
+    def __init__(self, label):
+        self.label = label
+        self.switchIsOn = False
+        self.brightness = 0
 
     def turnOn(self):
         self.switchIsOn = True
@@ -19,6 +20,24 @@ class DimmerSwitch():
             self.brightness = self.brightness - 1
     #Дополнительный метод для отладки
     def show(self):
+        print('Label:', self.label)
         print('Switch is on?',  self.switchIsOn)
         print('Brightness is:', self.brightness)
 
+
+oDimmer1 = DimmerSwitch('Dimmer1')
+oDimmer1.turnOn()
+oDimmer1.raiseLevel()
+oDimmer1.raiseLevel()
+oDimmer1.show()
+
+
+oDimmer2 = DimmerSwitch('Dimmer2')
+oDimmer2.turnOn()
+oDimmer2.raiseLevel()
+oDimmer2.raiseLevel()
+oDimmer2.raiseLevel()
+oDimmer2.show()
+
+oDimmer3 = DimmerSwitch('Dimmer3')
+oDimmer3.show()
