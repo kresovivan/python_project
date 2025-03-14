af18f9bffa67b3ca80f89b25859a569b5977d6b6
kresov <kresovivan@mail.ru>
2025-03-14 22:53:10 +0300

    test python1

---
+++ mathe3.py
@@ -0,0 +1,22 @@
+try:
+    while True:
+        print('введи число: ')
+        Number1 = float(input())
+        print('Введи еще одно число: ')
+        Number2 = float(input())
+        print('А теперь выбери операцию (+,-,*,/): ')
+        Operator = input()
+        print('Результат = ')
+        if Operator == '+':
+            print(Number1 + Number2)
+        if Operator == '-':
+            print(Number1 - Number2)
+        if Operator == '*':
+            print(Number1 * Number2)
+        if Operator == '/':
+            if Number2 !=0:
+                print(Number1 / Number2)
+            else:
+                print('Невозможно вычислить')
+except:
+    print('Не число!')
