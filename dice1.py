57564e31a078b4d607845e3a3fb15e96f0b3e835
kresov <kresovivan@mail.ru>
2025-03-15 00:36:20 +0300

    test python1

---
+++ dice1.py
@@ -0,0 +1,33 @@
+import random, time
+
+print('Давай бросим кубики!')
+Attemp = 0
+YourNumber = 0
+MyNumber = 0
+
+#Игральные кубики
+while Attemp < 5:
+    Attemp = Attemp + 1
+    print(str(Attemp) + '. Раунд')
+    print('Твой бросок: ', end='')
+    Shoot1 = random.randint(1,6) #Твой бросок
+    time.sleep(2) #Ожидание 2 секунды
+    print(Shoot1)
+    print('Мой бросок: ', end='')
+    Shoot2 = random.randint(1,6) #Мой бросок
+    time.sleep(2)
+    print(Shoot2)
+    if Shoot1 > Shoot2:
+        YourNumber = YourNumber + 1
+    if Shoot1 < Shoot2:
+        MyNumber = MyNumber + 1
+    print(str(YourNumber) + ' и ' + str(MyNumber))
+    time.sleep(3)
+    print()
+#Вычисления
+if YourNumber > MyNumber:
+    print('Ты выиграл!')
+elif YourNumber < MyNumber:
+    print('Я выиграл!!!')
+else:
+    print('Ничья')
