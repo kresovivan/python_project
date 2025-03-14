b102c1346c5c561376bf6d8c9fd2db8d8163b889
kresov <kresovivan@mail.ru>
2025-03-15 01:48:32 +0300

    test python1

--- guess7.py
+++ guess7.py
@@ -2,29 +2,28 @@ import random
 
 def initGame():
     global Attemp, Input
-Secret = 'Я задумал число от 1 до 1000'
-Case = random.randint(1, 1000)
-print(Secret)
-Attemp = 0
-Input = 0
+    Secret = 'Я задумал число от 1 до 1000'
+    print(Secret)
+    Attemp = 0
+    Input = 0
 
 def playGame():
     global Attemp, Input
-Case = random.randint(1,1000)
-while Input != Case:
-    print('Угадай число: ', end='')
-    Input = int(input())
-    Attemp = Attemp + 1
-    if Input < Case:
-        print('Слишком маленькое!')
-    if Input > Case:
-        print('Слишком большое!')
-    if Input == Case:
-        print("Вы угадали число!")
+    Case = random.randint(1,1000)
+    while Input != Case:
+        print('Угадай число: ', end='')
+        Input = int(input())
+        Attemp = Attemp + 1
+        if Input < Case:
+            print('Слишком маленькое!')
+        if Input > Case:
+            print('Слишком большое!')
+        if Input == Case:
+            print("Вы угадали число!")
 
 def endGame():
     global Attemp
-print('Ты пробовал ' + str(Attemp) + ' раз.')
+    print('Ты пробовал ' + str(Attemp) + ' раз.')
 
 #Осонвная программа
 initGame()
