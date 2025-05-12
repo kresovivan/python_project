5a10b81c9ed8554f011b0459db8238b3aaa43624
kresov <kresovivan@mail.ru>
2025-05-12 23:48:04 +0300

    test python1

--- gradle1.py
+++ gradle1.py
@@ -1,17 +1,17 @@
 print("Введи число:   ", end="")
 Score = int(input())
 print("Это ", end="")
-if (Score) >= 0 and (Score) < 25:
+if 0 <= Score < 25:
     print("Ужасно (6)")
-if (Score) >= 25 and (Score) < 45:
+if 25 <= Score < 45:
     print("Плохо (5)")
-if (Score) >= 45 and (Score) < 65:
+if 45 <= Score < 65:
     print("Сойдет(4)")
-if (Score) >= 65 and (Score) < 80:
+if 65 <= Score < 80:
     print("Средне(3)")
-if (Score) >= 80 and (Score) < 90:
+if 80 <= Score < 90:
     print("Хорошо(2)")
-if (Score) >= 90 and (Score) <= 100:
+if 90 <= Score <= 100:
     print("Очень хорошо (1)")
-if (Score) > 100 or (Score) < 0:
+if Score > 100 or Score < 0:
     print("Неправильно(0)")
