930080d3a783bad547ddb23cfb358196ab38c85d
kresov <kresovivan@mail.ru>
2025-05-12 22:47:21 +0300

    test python1

--- grade1.py
+++ grade1.py
@@ -2,7 +2,7 @@
 print("Введи число: ", end='')
 Score = int(input())
 print("Это ", end='')
-if Score >=0 and Score < 25:
+if Score >0 and Score < 25:
     print('Ужасно (6)')
 if Score >=25 and Score < 45:
     print('Плохо (5)')
@@ -14,7 +14,7 @@ if Score >=80 and Score < 90:
     print('Хорошо(2)')
 if Score >=90 and Score <=100:
     print('Очень хорошо (1)')
-if Score > 100 or Score <0:
+if Score > 100 or Score < 0:
     print('Некорректная оценка(0)')
 
 
