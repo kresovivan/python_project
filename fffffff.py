c279bff492f56f66f3622fa8911292dbc5900982
kresov <kresovivan@mail.ru>
2025-05-13 00:05:51 +0300

    test python1

--- fffffff.py
+++ fffffff.py
@@ -1,5 +1,17 @@
-print("Введи число:   ", end=" ")
+print("Введи число:   ", end="")
 Score = int(input())
-print("Это ", end=" ")
-if Score > 155 or Score < 0:
+print("Это ", end="")
+if 0 <= Score < 25:
+    print("Ужасно (6)")
+if 25 <= Score < 45:
+    print("Плохо (5)")
+if 45 <= Score < 65:
+    print("Сойдет(4)")
+if 65 <= Score < 80:
+    print("Средне(3)")
+if 80 <= Score < 90:
+    print("Хорошо(2)")
+if 90 <= Score <= 100:
+    print("Очень хорошо (1)")
+if Score > 100 or Score < 0:
     print("Неправильно(0)")
