e5cc16d01a8da5b78a95a1d367b10d57be06665a
kresov <kresovivan@mail.ru>
2025-06-24 14:38:30 +0300

    test python1

---
+++ million1.py
@@ -0,0 +1,14 @@
+print("Какую сумму ты хочешь инвестировать: ")
+Capital = float(input())
+print("Процентная ставка: ")
+Percent = float(input())
+print("На какой срок ты вкладываешь деньги: ")
+Term=int(input())
+for Value in range(Term):
+    Fee = Capital * Percent / 100
+    Capital = Capital + Fee
+print("Так ты получишь " + str(Capital) + " рублей")
+if Capital < 1000000:
+    print("Но ты так не станешь миллионером")
+else:
+    print("Добро пожаловать в Клуб миллионеров!")
