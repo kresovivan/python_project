5b27e4f48e858f29badeacca35a88e894ac92378
kresov <kresovivan@mail.ru>
2025-06-25 16:32:35 +0300

    test python1

---
+++ monster4.py
@@ -0,0 +1,29 @@
+class Monster:
+    #Инициализация аттрибутов
+    def __init__(self, name, character):
+        self.Name = name
+        self.Character = character
+    #Метод
+    def Type(self):
+        return "Монстр"
+
+    def show(self):
+        print("Имя: " + self.Name)
+        print("Особенность: " + self.Character)
+        print("Тип: " + self.Type())
+
+
+class GMonster(Monster):
+    def Type(self):
+        return "Дух монстра"
+
+class SMonster(Monster):
+    def Type(self):
+        return "Душа монстра"
+#Основная программа
+Frank = Monster("Фрэнки","Необычный")
+Frank.show()
+Albert = GMonster("Альберт","Задумчивый")
+Albert.show()
+Sigmund = SMonster("Зигмунд","Веселый")
+Sigmund.show()
---
+++ monster5.py
@@ -0,0 +1,18 @@
+class Monster:
+    #Инициализация аттрибутов
+    def __init__(self, name, character):
+        self.Name = name
+        self.Character = character
+    #Метод
+    def Type(self):
+        return "Монстр"
+
+    def show(self):
+        print("Имя: " + self.Name)
+        print("Особенность: " + self.Character)
+        print("Тип: " + self.Type())
+
+
+class GMonster(Monster):
+    def Type(self):
+        return "Дух монстра"
