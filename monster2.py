1be617727eacff2ccad699801f564ac5714773ea
kresov <kresovivan@mail.ru>
2025-06-25 16:14:46 +0300

    test python1

---
+++ monster2.py
@@ -0,0 +1,13 @@
+class Monster:
+    #Инициализация аттрибутов
+    def __init__(self, name, character):
+        self.Name = name
+        self.Character = character
+    #Метод
+    def show(self):
+        print("Имя: " + self.Name)
+        print("Особенность: " + self.Character)
+
+#Основная программа
+Frank = Monster("Фрэнки","Необычный")
+Frank.show()
