a34c91f8fa17e44410ef2016939804f2e5d675f9
kresov <kresovivan@mail.ru>
2025-06-26 17:58:48 +0300

    test python1

--- hello4.py
+++ hello4.py
@@ -2,10 +2,8 @@
 from tkinter import *
 
 #Константы текста
-Answer = ["Супер", "Хорошо", "Так себе", "Плохо", "Ужасно", \
-          "Не скажу"]
-Diagnose = ["Это здорово!", "Это радует!", "Все возможно", "Это огорчает!" \
-            "Это плохо", "Раз ты так думаешь..."]
+Answer = ["Супер", "Хорошо", "Так себе", "Плохо", "Ужасно", "Не скажу"]
+Diagnose = ["Это здорово!", "Это радует!", "Все возможно", "Это огорчает!","Это плохо", "Раз ты так думаешь..."]
 
 #Функция события
 def buttonClick(Nr):
@@ -22,9 +20,9 @@ Display.place(x=80, y=10, width=160, height=30)
 Knopka = []
 for Nr in range (0, 6):
     Knopka.append(Button(Window, text=Answer[Nr]))
-for pos in range (0,3)
-    Knopka[pos].place(x=20, y=60+pos*40, width=120, heght=30)
-    Knopka[pos+3].place(x=160, y=60 + pos * 40, width=120, heght=30 )
+for pos in range (0,3):
+    Knopka[pos].place(x=20, y=60+pos*40, width=120, height=30)
+    Knopka[pos+3].place(x=160, y=60 + pos * 40, width=120, height=30 )
 
 # Индивидульная настройка событий
 Knopka[0].config(command=lambda: buttonClick(0))
