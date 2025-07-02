f1c9367e19b8c73f2ba181f0ab11db1bf5a63271
kresov <kresovivan@mail.ru>
2025-07-02 15:37:13 +0300

    test python1

--- favorite_languages.py
+++ favorite_languages.py
@@ -14,3 +14,11 @@ for name in favorite_languages.keys():
     print(name.title())
 
 
+friends = ["phil", "sarah"]
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
