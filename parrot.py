6ec375765bb4436a3690b8c67cf27fff17c0d746
kresov <kresovivan@mail.ru>
2025-07-03 15:01:48 +0300

    test python1

--- parrot.py
+++ parrot.py
@@ -1,2 +1,6 @@
-message = input("Tell me something, and I will repeat it back to you: ")
-print(message)
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'q' to end the program. "
+message = "\n"
+while message != 'q':
+    message = input(prompt)
+    print(message)
