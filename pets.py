a4bb05cbbb177dae6103219761ba46829e2a2f4a
kresov <kresovivan@mail.ru>
2025-07-04 17:19:32 +0300

    test python1

--- pets.py
+++ pets.py
@@ -1,6 +1,6 @@
-pets = ['dog', 'cat', 'dog', 'goldfish','cat', 'rabbit', 'cat']
-print(pets)
+def describe_pet(animal_type, pet_name):
+    """Выводит информацию о животном"""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
 
-while 'cat' in pets:
-    pets.remove('cat')
-print(pets)
+describe_pet('hamster', 'harry')
