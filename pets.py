ac9cba93aad0f58767b6d8093b1f5549381f447e
kresov <kresovivan@mail.ru>
2025-07-07 17:40:31 +0300

    test python1

--- pets.py
+++ pets.py
@@ -1,4 +1,4 @@
-def describe_pet(animal_type, pet_name):
+def describe_pet(pet_name, animal_type='cat'):
     """Выводит информацию о животном"""
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
@@ -8,4 +8,4 @@ describe_pet('hamster', 'harry')
 describe_pet('dog', 'sharik')
 
 
-describe_pet(animal_type= 'cat', pet_name='kisunchik')
+describe_pet(pet_name='kisunchik')
