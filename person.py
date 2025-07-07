d07e405d0e7f7a0dfeb1f1a9dc56ef278cd2288e
kresov <kresovivan@mail.ru>
2025-07-07 20:58:01 +0300

    test python1

--- person.py
+++ person.py
@@ -1,6 +1,8 @@
-def build_person(first_name, last_name):
+def build_person(first_name, last_name, age=None):
     """Возвращает словарь с информацией о человеке."""
     person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
     return person
 
 musician = build_person('jimi', 'hendrix')
