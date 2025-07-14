5f97aeb8d6d65750fe4083529750d0b9dfd8d4ba
kresov <kresovivan@mail.ru>
2025-07-14 17:15:22 +0300

    test python1

--- greeter.py
+++ greeter.py
@@ -1,5 +1,12 @@
-def greet_user(username):
-    """Выводит простое приветcтвие"""
-    print(f"Hello {username.title()}")
+def get_formatted_name(first_name, last_name):
+    """Возвращает аккуратно отформатированное полное имя"""
+    full_name = f"{first_name} {last_name}"
+    return  full_name.title()
 
-greet_user("ivan")
+while True:
+    print("\nPlease tell your name: ")
+    f_name = input("First name: ")
+    l_name = input("Last name: ")
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
