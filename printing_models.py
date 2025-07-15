36711699c5244bab34ffd129a0679a497459415f
kresov <kresovivan@mail.ru>
2025-07-15 20:38:54 +0300

    test python1

--- printing_models.py
+++ printing_models.py
@@ -1,18 +1,22 @@
-#Cписок моделей, которые необходимо напечатать на принтере
-unprinted_designs = ["phone case", "robot pendant", "dodecahedron"]
+def print_models(unprinted_designs, completed_models):
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"printing model: {current_design}")
+        completed_models.append(current_design)
+"""Цикл последовательно печатает каждую модель до конца списка. 
+После печати каждая модель перемещается в список completed_models."""
+
+
+def show_completed_models(completed_models):
+    print("\nThe following models have been printed")
+    for completed_model in completed_models:
+        print(completed_model)
+"""Выводит информацию обо всех напечатанных моделях"""
+
+unprinted_designs = ["phone_case","robot","shoes"]
 completed_models = []
 
-def print_models(unprinted_designs, completed_models):
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
 
-#Цикл последовательно печатает каждую модель до конца списка.
-#После печати каждая модель перемещается в список completed_models.
-while unprinted_designs:
-    current_design = unprinted_designs.pop()
-    print(f"printing model: {current_design}")
-    completed_models.append(current_design)
-
-#Вывод всех готовых моделей.
-print("\nThe following models have been printed")
-for completed_model in completed_models:
-    print(completed_model)
 
